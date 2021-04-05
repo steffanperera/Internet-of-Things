7cfe5b7079f9e7a2e11d7d26c9dbe0cacca90969
trill97 <59191481+trill97@users.noreply.github.com>
2021-04-05 13:42:07 +0530

    Update and rename index.html to index.php

---
+++ website/index.php
@@ -0,0 +1,31 @@
+<?php include('includes/headerindex.php'); ?>
+	  <br>
+	  
+	  <div class="container">
+    <div class="row">
+        <div class="col-md-offset-5 col-md-4 text-center offset-lg-4 col-lg-4">
+            
+              <div class="form-login"><br>
+                <img src="images/logo2.png" width="50" height="62" alt=""/></a>
+                <h4>LOGIN</h4>
+                
+                <br>
+                <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Username"/>
+                <br><br>
+                <input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="Password"/>
+                <br><br>
+                <div class="wrapper">
+                        <span class="group-btn">
+                            <a href="#" class="btn btn-danger btn-md">login <i class="fa fa-sign-in"></i></a>
+                        </span>
+                </div>
+            </div>
+        </div>
+    </div>
+		  
+	</div>
+	  <br><br><br>
+	  
+	  <div></div>
+<?php include('includes/footer.php'); ?>
+  
